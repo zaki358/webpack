4898bd3127dea0fa4acd7ebe1f543bd0aca2c9b4
zaki358 <t.yamazaki.1129@gmail.com>
2022-09-08 00:39:34 +0900

    html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -38,16 +38,36 @@ module.exports = {
                },
             ],
          },
+         {
+            test: /\.pug/,
+            use: [
+               {
+                  loader: 'html-loader',
+               },
+               {
+                  loader: 'pug-html-loader',
+                  options: {
+                    pretty: true,
+                  },
+               },
+            ],
+         },
       ],
    },
    plugins: [
       new MiniCssExtractPlugin({
+        //ビルドされたほうの名前
          filename: './css/main.css',
       }),
+      //このhtmlファイルが親みたいなもので上記のビルドされた内容（プラグインやライブラリ）が読み込まれる
       new HtmlWebpackPlugin({
-         //このhtmlファイルが親みたいなもので上記のビルドされた内容（プラグインやライブラリ）が読み込まれる
-         template: './src/templates/index.html',
+         template: './src/templates/index.pug',
+         filename: 'index.html',
       }),
+      new HtmlWebpackPlugin({
+        template: './src/templates/access.pug',
+        filename: 'access.html',
+     }),
       new CleanWebpackPlugin(),
    ],
 }
